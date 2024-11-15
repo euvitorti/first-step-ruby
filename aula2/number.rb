1b97520eb6964ad2a732d01a326ceafc0814688d
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-11-15 17:20:49 -0300

    Operation

--- aula2/number.rb
+++ aula2/number.rb
@@ -1,6 +1,23 @@
-x = 10
-y = 5
+# Operations
+# (+,-,*,/)
 
-soma = x + y
+# the number with out (.) it is an int, with (.) it is float, for example: 10.0 .
 
-puts singleton_method_added
+puts "Enter one number"
+x = gets.chomp.to_i
+
+puts "Enter another number"
+y = gets.chomp.to_i
+
+sum = x + y
+subtract = x + y
+multiply = x * y
+divide = x.to_f / y.to_f
+
+puts "Sum: " + sum.to_s
+puts "Subtract: " + subtract.to_s
+puts "Multiply: " + multiply.to_s
+puts "DIvide: " + divide.to_s
+
+
+puts "Ruby\n" * 5
